b9ef9bca1ff109a9e4a0ad1e08301eb352c124fe
吴沿江 <616352178@qq.com>
2017-09-25 10:12:37 +0800

    去除沉余代码

--- src/main/java/com/test/service/impl/EmailServiceImpl.java
+++ src/main/java/com/test/service/impl/EmailServiceImpl.java
@@ -3,7 +3,6 @@ package com.test.service.impl;
 import java.util.List;
 
 import javax.annotation.Resource;
-import javax.management.RuntimeErrorException;
 
 import org.springframework.stereotype.Service;
 
