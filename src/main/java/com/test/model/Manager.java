64724b27fba445e93307cf88d342bc85104fd1fd
heqiyong <1220249839@qq.com>
2018-03-20 11:58:28 +0800

    何其勇MAVEN lombok依赖

---
+++ src/main/java/com/test/model/Manager.java
@@ -0,0 +1,14 @@
+package com.test.model;
+
+import java.util.Date;
+
+public class Manager {
+ private String id;
+ private String magr_no;
+ private String password;
+ private String mgr_name;
+ private String groups_id;
+ private Date last_login_time;
+ private Date create_time;
+
+}
---
+++ src/main/java/com/test/model/News.java
@@ -0,0 +1,13 @@
+package com.test.model;
+
+import java.util.Date;
+
+public class News {
+    private String id;
+    private String url;
+    private String type;
+    private Integer access_count;
+    private Integer  status;
+    private Date datetime;
+
+}
