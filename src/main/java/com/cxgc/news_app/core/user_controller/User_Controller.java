b718f2211742ae06fe279d122c088daecb14134a
ly <1414281309@qq.com>
2018-03-20 15:50:51 +0800

    添加了用户包结构及类

---
+++ src/main/java/com/cxgc/news_app/core/user_controller/User_Controller.java
@@ -0,0 +1,20 @@
+package com.cxgc.news_app.core.user_controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 用户操作
+ * 流转控制层
+ */
+@Controller
+public class User_Controller {
+
+
+    @RequestMapping("/user")
+    public Object login(String phone){
+        System.out.println("phone"+phone);
+        return null;
+    }
+}
---
+++ src/main/java/com/cxgc/news_app/core/user_mapper/User_Mapper.java
@@ -0,0 +1,11 @@
+package com.cxgc.news_app.core.user_mapper;
+
+import com.cxgc.news_app.core.model.User;
+
+/**
+ * 用户数据交互层
+ */
+public interface User_Mapper {
+
+    public User getUser(Integer id);
+}
