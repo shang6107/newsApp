d73b4a3222978e15b58dd0c504c5009e1a6f739d
shangguan <3133891366@qq.com>
2018-03-21 17:06:08 +0800

    上官--     创建 manager dao

---
+++ src/main/java/com/cxgc/news_app/core/services/managerment_service/ManagerService.java
@@ -0,0 +1,69 @@
+package com.cxgc.news_app.core.services.managerment_service;
+
+import com.cxgc.news_app.core.model.Manager;
+
+import java.util.Map;
+
+/**
+ *  @author 何其勇
+ *  @Version
+ *  @Description
+ */
+public interface ManagerService {
+    /**
+     * 管理员登录
+     * @param mgrNo
+     * @param password
+     * @return 集合
+     */
+
+    public Map<String,Object> ManagerLogin(String mgrNo, String password);
+
+
+    /**
+     * 添加管理员
+     * @param manager
+     * @return
+     */
+
+    public Map<String,Object> addManager(Manager manager);
+
+/**
+ * 根据ID删除管理员
+ * @param id
+ * @return
+ */
+
+    public Map<String,Object> deleteManagerById(int id);
+
+
+/**
+ * 获取所有管理员
+ * @return
+ */
+
+    public Map<String,Object> getManager();
+
+
+/**
+ * 修改管理员基本信息
+ * @param manager
+ * @return
+ */
+
+    public Map<String,Object> modifyManagerInfo(Manager manager);
+
+
+/**
+ * 通过管理员ID修改管理员密码
+ * @param id
+ * @param password
+ * @return
+ */
+
+    public Map<String,Object> modifyPasswordById(int id, String password);
+
+}
+
+
+
