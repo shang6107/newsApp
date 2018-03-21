8ba7021beec513aa716337569bebc2d80dc962a6
ly <1414281309@qq.com>
2018-03-21 17:53:15 +0800

    添加了短信业务类

--- src/main/java/com/cxgc/news_app/core/mapper/user_mapper/User_Mapper.java
+++ src/main/java/com/cxgc/news_app/core/mapper/user_mapper/User_Mapper.java
@@ -6,6 +6,32 @@ import com.cxgc.news_app.core.model.User;
  * 用户数据交互层
  */
 public interface User_Mapper {
+    /**
+     * 通过id获取用户信息
+     * @param id
+     * @return
+     */
+    public User getUserById(Integer id);
 
-    public User getUser(Integer id);
+    /**
+     * 通过手机号获取用户信息
+     * @param phone
+     * @return
+     */
+    public User getUserByPhone(String phone);
+
+    /**
+     * 修改用户信息
+     * @param user
+     * @return
+     */
+    public Integer editUserInfo(User user);
+
+
+    /**
+     * 新增用户
+     * @param user
+     * @return
+     */
+    public Integer addUser(User user);
 }
---
+++ src/main/java/com/cxgc/news_app/core/user_service/User_Service.java
@@ -0,0 +1,24 @@
+package com.cxgc.news_app.core.user_service;
+
+
+import com.cxgc.news_app.core.model.User;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+
+/**
+ * 用户
+ * 业务层
+ */
+@Service
+public class User_Service {
+
+
+
+    public User getUser(Integer id){
+
+
+        return null;
+    }
+
+
+}
