d501c5836d9f9f5fb1252406ee4862a9d3e705b9
唐倩 <1327423216@qq.com>
2018-03-21 17:31:18 +0800

    新闻页面的service层

---
+++ src/main/java/com/cxgc/news_app/core/services/news_service/imple/NewsService.java
@@ -0,0 +1,7 @@
+package com.cxgc.news_app.core.services.news_service.imple;
+
+/**
+ * Created by jiang_hai on 2018/3/21.
+ */
+public interface NewsService {
+}
---
+++ src/main/java/com/cxgc/news_app/core/services/news_service/imple/imple/NewsServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cxgc.news_app.core.services.news_service.imple.imple;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jiang_hai on 2018/3/21.
+ */
+@Service
+public class NewsServiceImpl {
+}
