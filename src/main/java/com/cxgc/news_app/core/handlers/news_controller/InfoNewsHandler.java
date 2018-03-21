37b34e542ac0f7bed118b88a755eeaeb933b11a3
唐倩 <1327423216@qq.com>
2018-03-21 16:52:46 +0800

    处理查看新闻详细，评论新闻以及对评论点赞的控制层

---
+++ src/main/java/com/cxgc/news_app/core/handlers/news_controller/InfoNewsHandler.java
@@ -0,0 +1,11 @@
+package com.cxgc.news_app.core.handlers.news_controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by tangqian on 2018/3/21.
+ *
+ */
+@Controller
+public class InfoNewsHandler {
+}
