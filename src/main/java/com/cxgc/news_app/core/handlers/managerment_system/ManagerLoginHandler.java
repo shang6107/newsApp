4064cb26db223c98c46d47a47695def7572376da
shangguan <3133891366@qq.com>
2018-03-22 17:20:18 +0800

    111

--- src/main/java/com/cxgc/news_app/core/handlers/managerment_system/ManagerLoginHandler.java
+++ src/main/java/com/cxgc/news_app/core/handlers/managerment_system/ManagerLoginHandler.java
@@ -2,10 +2,8 @@ package com.cxgc.news_app.core.handlers.managerment_system;
 
 import com.cxgc.news_app.core.model.Manager;
 import com.cxgc.news_app.core.services.managerment_service.ManagerService;
-import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
