97dbec419520c9099f3cf22e917a044c4047604d
唐倩 <1327423216@qq.com>
2018-03-23 17:20:29 +0800

    获得指定新闻的所有评论

--- src/main/java/com/cxgc/news_app/core/services/news_service/imple/NewsService.java
+++ src/main/java/com/cxgc/news_app/core/services/news_service/imple/NewsService.java
@@ -1,5 +1,9 @@
 package com.cxgc.news_app.core.services.news_service.imple;
 
+import com.cxgc.news_app.core.model.Comment;
+
+import java.util.Collection;
+
 /**
  * Created by 唐倩 on 2018/3/21.
  */
@@ -8,4 +12,8 @@ public interface NewsService {
      * 通过新闻id获得新闻的本地地址
      */
     String getNewsById(String id);
+    /**
+     * 通过新闻id获得新闻对象，包括评论等信息
+     */
+    Collection<Comment> getAllCommentByNewsId(String id);
 }
