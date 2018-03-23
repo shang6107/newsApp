187e1b1425d116823ac1965d2b439b2e2ebafb35
唐倩 <1327423216@qq.com>
2018-03-23 17:39:36 +0800

    控制层方法优化

--- src/main/java/com/cxgc/news_app/core/handlers/news_handler/InfoNewsHandler.java
+++ src/main/java/com/cxgc/news_app/core/handlers/news_handler/InfoNewsHandler.java
@@ -3,6 +3,7 @@ package com.cxgc.news_app.core.handlers.news_handler;
 
 import com.cxgc.news_app.core.model.Comment;
 import com.cxgc.news_app.core.services.news_service.imple.NewsService;
+import com.cxgc.news_app.utility.news.NewsIO;
 import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -27,30 +28,22 @@ public class InfoNewsHandler {
     @Autowired
     private NewsService newsService;
     /**
-     * 通过新闻id获得新闻的本地地址
+     * 通过新闻id获得新闻及所有评论
      * @return
      */
     @RequestMapping("/getOneNews")
-    public Map<String,Object> test(String id, HttpServletResponse response) throws IOException {
-        System.out.println("id = " + id);
+    public Map<String,Object> test(String id) throws IOException {
+        Map<String,Object> map=new HashedMap();
+
         String url = newsService.getNewsById(id);
-        System.out.println("url = " + url);
         if(url==null){
             return null;
         }
-            File file=new File(url);
-            StringBuffer stringBuffer=new StringBuffer();
-            InputStream is=new FileInputStream(file);
-            int len;
-            byte[] b=new byte[1024];
-            while((len=is.read(b))!=-1){
-                stringBuffer.append(b.toString());
-            }
-            System.out.println("stringBuffer = " + stringBuffer);
+            StringBuffer stringFromFile = NewsIO.getStringFromFile(url);
             //获得该新闻的所有评论
             Collection<Comment> commentCollection = newsService.getAllCommentByNewsId(id);
-            Map<String,Object> map=new HashedMap();
-            map.put("news",stringBuffer);
+
+            map.put("news",stringFromFile);
             map.put("comments",commentCollection);
             return map;
     }
