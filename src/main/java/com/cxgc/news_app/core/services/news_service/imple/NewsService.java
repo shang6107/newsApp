781ca425e9a2320d64561d621f3e482d117ddb55
唐倩 <1327423216@qq.com>
2018-03-23 15:18:30 +0800

    service层获得新闻数据的方法

--- src/main/java/com/cxgc/news_app/core/services/news_service/imple/NewsService.java
+++ src/main/java/com/cxgc/news_app/core/services/news_service/imple/NewsService.java
@@ -4,4 +4,8 @@ package com.cxgc.news_app.core.services.news_service.imple;
  * Created by 唐倩 on 2018/3/21.
  */
 public interface NewsService {
+    /**
+     * 通过新闻id获得新闻的网络地址
+     */
+    String getNewsById(String id);
 }
