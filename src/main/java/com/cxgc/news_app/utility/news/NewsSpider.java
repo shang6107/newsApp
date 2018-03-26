4ab059f209ee03b1ac8e841008734e3acb76f014
xyz <isooso>
2018-03-26 16:22:57 +0800

    新闻表添加新闻标题属性

--- src/main/java/com/cxgc/news_app/utility/news/NewsSpider.java
+++ src/main/java/com/cxgc/news_app/utility/news/NewsSpider.java
@@ -14,7 +14,6 @@ import java.util.UUID;
  */
 public class NewsSpider {
 
-
     public static void main(String[] args) throws IOException {
 
         /*NewsSpider n = new NewsSpider();
@@ -26,12 +25,11 @@ public class NewsSpider {
 
         URLConnection urlConnection = url.openConnection();
 
-        /*urlConnection.setRequestProperty("Content-Type","application/json");*/
         InputStream inputStream = urlConnection.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("utf-8")));
 
         StringBuffer sb = new StringBuffer();
-        String l = null;
+        String l;
 
         while ((l=br.readLine())!=null){
             sb.append(l);
@@ -39,38 +37,4 @@ public class NewsSpider {
         System.out.println(sb.toString());
         return null;
     }
-
-   /* private static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
-        }
-        return sb.toString();
-    }
-
-    public static JSONObject getRequestFromUrl(String url) throws IOException, JSONException {
-        URL realUrl = new URL(url);
-        URLConnection conn = realUrl.openConnection();
-        conn.setRequestProperty("Content-type", "application/json");
-        InputStream instream = conn.getInputStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(instream, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            System.out.println(jsonText);
-            JSONObject json = new JSONObject();
-            json.getJSONObject(jsonText);
-            return json;
-        } finally {
-            instream.close();
-        }
-    }
-    public static void main(String[] args) throws IOException, JSONException {
-
-// 请求示例 url 默认请求参数已经做URL编码
-        String url = "http://120.76.205.241:8000/news/cctvplus?catid=10121&apikey=nsnWYSTqcyapnggHrpwHIhhmKtg2Z2nK4ZggjXHiWjWTiFPZyLGDeg7SjvOQAXps";
-
-        JSONObject json = getRequestFromUrl(url);
-        System.out.println(json.toString());
-    }*/
 }
