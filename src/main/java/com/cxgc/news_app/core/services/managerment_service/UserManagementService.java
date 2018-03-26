4968297bfaa3894521def698f877cbbcc5afd9de
heqiyong <1220249839@qq.com>
2018-03-26 17:00:31 +0800

    用户页面展示

--- src/main/java/com/cxgc/news_app/core/services/managerment_service/UserManagementService.java
+++ src/main/java/com/cxgc/news_app/core/services/managerment_service/UserManagementService.java
@@ -1,19 +1,34 @@
-/*
 package com.cxgc.news_app.core.services.managerment_service;
 
 import com.cxgc.news_app.core.model.User;
 
 import java.util.List;
 
-*/
 /**
  * @author 何其勇
  * @Date
  * @Version
  * @Description
- *//*
+ */
 
 public interface UserManagementService {
-List<User> selectAllUser();
+ /**
+  * 查询所有用户
+  * @return 用户集合
+  */
+ List<User> selectAllUser();
+
+ /**
+  * 修改用户状态
+  * @param id
+  * @return
+  */
+ User updateUser(String id);
+
+ /**
+  * 获得用户的信息通过ID
+  * @param id
+  * @return
+  */
+ User getUserById(String id);
 }
-*/
