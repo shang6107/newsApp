45ea729c12ae3f72c71dbb2573defbb858f8ec69
唐倩 <1327423216@qq.com>
2018-03-28 09:13:20 +0800

    时间字段类型更正

--- src/main/java/com/cxgc/news_app/core/model/Collections.java
+++ src/main/java/com/cxgc/news_app/core/model/Collections.java
@@ -2,6 +2,8 @@ package com.cxgc.news_app.core.model;
 
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * @author 何其勇
  * @Date 2018-03-20 / 01:57:41
@@ -14,5 +16,5 @@ public class Collections {
     private String releaseId;
     private String newsId;
     private String userId;
-    private String createTime;
+    private Date createTime;
 }
