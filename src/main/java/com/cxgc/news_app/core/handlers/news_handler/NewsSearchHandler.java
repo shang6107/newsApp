fa5a55bb9d934ea823f61fb98c68e40c1da91273
xyz <isooso@163.com>
2018-04-01 18:04:46 +0800

    新闻搜索，通过异步进行数据库搜索和百度搜索结果页面的数据处理，添加了一个jsoup架包

---
+++ src/main/java/com/cxgc/news_app/core/handlers/news_handler/NewsSearchHandler.java
@@ -0,0 +1,32 @@
+package com.cxgc.news_app.core.handlers.news_handler;
+
+import com.cxgc.news_app.core.model.News;
+import com.cxgc.news_app.core.services.news_service.NewsSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 徐瑜壮
+ * @Date 2018-04-01 / 04:46:28
+ */
+@Controller
+@RequestMapping("/app")
+public class NewsSearchHandler {
+    @Autowired
+    private NewsSearchService nss;
+
+    @RequestMapping("newsSearch")
+    @ResponseBody
+    public Map<String, List<News>> newsSearch(String search) {
+        Map<String, List<News>> map = new HashMap<>();
+        map.put("newsSearch", nss.newsSearch(search));
+        return map;
+    }
+}
