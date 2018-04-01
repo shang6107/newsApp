87e21d97bde3a3de44d12e216bef7a7c5a63f33a
shangguan <3133891366@qq.com>
2018-04-01 15:01:17 +0800

    应用程序数据统计监听器

---
+++ src/main/java/com/cxgc/news_app/core/handlers/managerment_system/BasicAjaxHandler.java
@@ -0,0 +1,47 @@
+package com.cxgc.news_app.core.handlers.managerment_system;
+
+import com.cxgc.news_app.core.services.managerment_service.NewsManagermentService;
+import com.cxgc.news_app.core.services.managerment_service.UserManagementService;
+import static com.cxgc.news_app.utility.RequestParameterWrapUtil.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.ParseException;
+
+/**
+ * @author 上官炳强
+ * @Date 2018-03-31 / 11:23:24
+ * @Version
+ * @Description ajax 数据请求专用接口
+ */
+@Controller
+@RequestMapping("/ajax")
+public class BasicAjaxHandler {
+    private UserManagementService userManagementService;
+    private NewsManagermentService newsManagermentService;
+
+    @Autowired
+    public void setUserManagementService(UserManagementService userManagementService) {
+        this.userManagementService = userManagementService;
+    }
+    @Autowired
+    public void setNewsManagermentService(NewsManagermentService newsManagermentService) {
+        this.newsManagermentService = newsManagermentService;
+    }
+
+    @RequestMapping("/user-list")
+    @ResponseBody
+    public Object userList(Integer pageNum , Integer pageSize) throws ParseException, IllegalAccessException {
+        return $(userManagementService.selectAllUser(pageNum,pageSize));
+    }
+
+    @RequestMapping("/user-info/{phoneNum}")
+    @ResponseBody
+    public Object getUserByPhone(@PathVariable("phoneNum") String phoneNum) throws ParseException, IllegalAccessException {
+        return $(userManagementService.getUserByPhoneNum(phoneNum));
+    }
+}
